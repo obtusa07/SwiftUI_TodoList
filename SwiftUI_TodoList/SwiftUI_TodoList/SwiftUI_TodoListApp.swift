4e641a22133b73ec39dfaa6ee93ef908fec29c35
Taehwan Kim <obtusa07@gmail.com>
2022-09-12 17:00:13 +0900

    feat: init project
    Create Project

---
+++ SwiftUI_TodoList/SwiftUI_TodoList/SwiftUI_TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TodoListApp.swift
+//  SwiftUI_TodoList
+//
+//  Created by Taehwan Kim on 2022/09/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
