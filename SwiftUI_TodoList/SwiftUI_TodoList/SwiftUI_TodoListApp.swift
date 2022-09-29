242a04b30e3c814e4bb3acecc507339b29ab7991
Taehwan Kim <obtusa07@gmail.com>
2022-09-29 21:59:05 +0900

    feat: support iPad

--- SwiftUI_TodoList/SwiftUI_TodoList/SwiftUI_TodoListApp.swift
+++ SwiftUI_TodoList/SwiftUI_TodoList/SwiftUI_TodoListApp.swift
@@ -24,6 +24,7 @@ struct SwiftUI_TodoListApp: App {
             NavigationView {
                 ListView()
             }
+            .navigationViewStyle(StackNavigationViewStyle())
             .environmentObject(listViewModel)
         }
     }
